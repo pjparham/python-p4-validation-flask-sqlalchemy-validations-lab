988a9e03c244418ac31d33f6d22e49a49bf63584
Parker Parham <parkerparham@gmail.com>
2023-05-18 17:09:29 -0400

    Adds validations for Post and Author tables

--- server/models.py
+++ server/models.py
@@ -12,6 +12,19 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Author must have name attribute")
+        return name
+
+    @validates('phone_number')
+    def validate_number(self, key, number):
+       if len(number) != 10:
+           raise ValueError("Phone number must be 10 digists")
+       return number
+
+
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -27,6 +40,31 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('content')
+    def validates_content_length(self, key, content):
+        if len(content) <= 250:
+            raise ValueError("Post Content must be at least 250 characters long.")
+        return content
+    
+    @validates('summary')
+    def validates_summary_length(self, key, summary):
+        if len(summary) >= 250:
+            raise ValueError("Post summary cannot exceed 250 characters.")
+        return summary
+
+    @validates('category')
+    def validates_category(self, key, category):
+        categories = ["Fiction", "Non-Fiction"]
+        if category not in categories:
+            raise ValueError("Category must be either Fiction or Non-Fiction")
+        return category
+    
+    @validates('title')
+    def clickbait(self, key, title):
+        clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(substring in title for substring in clickbait):
+            raise ValueError("No clickbait found")
+        return title
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
